edc6ad2440cd73b2dedf1cde46f7961710a65e5f
thanhvanvt123 <lethanhvan0105@gmail.com>
2020-11-18 10:26:26 +0700

    add recursion and still see GREEN again

--- src/van/util/MathUtility.java
+++ src/van/util/MathUtility.java
@@ -24,19 +24,30 @@ public class MathUtility {
     //chặn giai thừa từ 0 đến 20
     //âm hoặc 21 bỏ ko tính
     
+//    public static long getFactorial(int n) {
+//        if(n < 0 || n > 20) {
+//            throw new IllegalArgumentException("n must be between 0 ... 20");
+//        } 
+//        if(n == 0 || n == 1){
+//            return 1; //dac biet return luon
+//        } 
+//        //dua nao viết else ở đây trừ điểm
+//        //mặc nhiên CPU đến đc đây là hiểu rằng n = 2...20
+//        long result = 1;
+//        for (int i = 2; i <= n; i++) {
+//            result *= i;
+//        }
+//        return result;
+//    }
+    
+    //hàm đệ quy
     public static long getFactorial(int n) {
-        if(n < 0 || n > 20) {
-            throw new IllegalArgumentException("n must be between 0 ... 20");
-        } 
-        if(n == 0 || n == 1){
-            return 1; //dac biet return luon
-        } 
-        //dua nao viết else ở đây trừ điểm
-        //mặc nhiên CPU đến đc đây là hiểu rằng n = 2...20
-        long result = 1;
-        for (int i = 2; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+        if(n < 0 || n > 20) 
+            throw new IllegalArgumentException("n must be between 0 ..20");
+        if(n == 0 || n == 1) 
+            return 1;
+        return n * getFactorial(n - 1);
+        //gọi lại chính hàm của mình vs qui mô khác , n - 1
+        //đi mãi sẽ về 1
     }
 }
